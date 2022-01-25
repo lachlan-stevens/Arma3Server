bac4434b4a40b478e410932fd336736f2e23efa1
Lachlan Stevens <stevens.lachlan@gmail.com>
2022-01-25 17:43:17 +1100

    Added env variables to deal with seperating server and client in seperate nodes, as well as handle legacy syntax.

--- launch.py
+++ launch.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess
+import ipaddress
 
 import local
 import workshop
@@ -15,7 +16,9 @@ def env_defined(key):
 
 
 CONFIG_FILE = os.environ["ARMA_CONFIG"]
+OP_MODE = os.environ["OP_MODE"]
 KEYS = "/arma3/keys"
+HEADLESS_IP = os.environ["HOST_IP"]
 
 if not os.path.isdir(KEYS):
     if os.path.exists(KEYS):
@@ -57,11 +60,73 @@ if os.environ["ARMA_CDLC"] != "":
     for cdlc in os.environ["ARMA_CDLC"].split(";"):
         launch += " -mod={}".format(cdlc)
 
-clients = int(os.environ["HEADLESS_CLIENTS"])
-print("Headless Clients:", clients)
+print("Starting container in {} mode".format(OP_MODE)
 
-if clients != 0:
-    with open("/arma3/configs/{}".format(CONFIG_FILE)) as config:
+clients = int(os.environ["HEADLESS_CLIENTS"])
+
+if OP_MODE.lower() = "basic":   
+
+    clients = int(os.environ["HEADLESS_CLIENTS"])
+    print("Headless Clients:", clients)
+    
+    if clients != 0:
+        with open("/arma3/configs/{}".format(CONFIG_FILE)) as config:
+            data = config.read()
+            regex = r"(.+?)(?:\s+)?=(?:\s+)?(.+?)(?:$|\/|;)"
+
+            config_values = {}
+
+            matches = re.finditer(regex, data, re.MULTILINE)
+            for matchNum, match in enumerate(matches, start=1):
+                config_values[match.group(1).lower()] = match.group(2)
+
+            if "headlessclients[]" not in config_values:
+                data += '\nheadlessclients[] = {"127.0.0.1"};\n'
+            if "localclient[]" not in config_values:
+                data += '\nlocalclient[] = {"127.0.0.1"};\n'
+
+            with open("/tmp/arma3.cfg", "w") as tmp_config:
+                tmp_config.write(data)
+            launch += ' -config="/tmp/arma3.cfg" -cfg="/arma3/configs/{}"'.format(BASIC_CONFIG_FILE)
+
+        client_launch = launch
+        client_launch += " -client -connect=127.0.0.1"
+        if "password" in config_values:
+            client_launch += " -password={}".format(config_values["password"])
+
+        for i in range(0, clients):
+            hc_launch = client_launch + ' -name="{}-hc-{}"'.format(
+                os.environ["ARMA_PROFILE"], i
+            )
+            print("LAUNCHING ARMA CLIENT {} WITH".format(i), hc_launch)
+            subprocess.Popen(hc_launch, shell=True)
+
+    else:
+        launch += ' -config="/arma3/configs/{}" -cfg="/arma3/configs/{}"'.format(CONFIG_FILE, BASIC_CONFIG_FILE)
+
+    launch += ' -port={} -name="{}" -profiles="/arma3/configs/profiles"'.format(
+    os.environ["PORT"], os.environ["ARMA_PROFILE"]
+)
+
+    if os.path.exists("servermods"):
+        launch += mod_param("serverMod", local.mods("servermods"))
+
+
+
+    print("LAUNCHING ARMA SERVER WITH", launch, flush=True)
+        os.system(launch)
+
+else if OP_MODE.lower() = "standalone":
+    
+    if (clients > 0):
+        print("[WARNING]: HEADLESS_CLIENTS set to {}. Expected for standalone mode: 0. Ignoring.".format{clients})
+
+    if HEADLESS_IP != "":
+        headless_list = []
+        for ip in HEADLESS_IP.split(";")
+            headless_list+= ipaddress.ip_network(ip).hosts()
+
+        with open("/arma3/configs/{}".format(CONFIG_FILE)) as config:
         data = config.read()
         regex = r"(.+?)(?:\s+)?=(?:\s+)?(.+?)(?:$|\/|;)"
 
@@ -71,36 +136,68 @@ if clients != 0:
         for matchNum, match in enumerate(matches, start=1):
             config_values[match.group(1).lower()] = match.group(2)
 
+        config_ip = ""
+        for ip in headless_list:
+            config_ip += ' {"{ip}"}'.replace('{ip}', str(ip))
+
         if "headlessclients[]" not in config_values:
-            data += '\nheadlessclients[] = {"127.0.0.1"};\n'
+            data += '\nheadlessclients[] ={};\n'.format(config_ip)
         if "localclient[]" not in config_values:
-            data += '\nlocalclient[] = {"127.0.0.1"};\n'
+            data += '\nlocalclient[] = {};\n'.format(config_ip)
 
         with open("/tmp/arma3.cfg", "w") as tmp_config:
             tmp_config.write(data)
-        launch += ' -config="/tmp/arma3.cfg"'
+        launch += ' -config="/tmp/arma3.cfg" -cfg="/arma3/configs/{}"'.format(BASIC_CONFIG_FILE)
 
-    client_launch = launch
-    client_launch += " -client -connect=127.0.0.1"
-    if "password" in config_values:
-        client_launch += " -password={}".format(config_values["password"])
-
-    for i in range(0, clients):
-        hc_launch = client_launch + ' -name="{}-hc-{}"'.format(
-            os.environ["ARMA_PROFILE"], i
-        )
-        print("LAUNCHING ARMA CLIENT {} WITH".format(i), hc_launch)
-        subprocess.Popen(hc_launch, shell=True)
-
-else:
-    launch += ' -config="/arma3/configs/{}" -cfg="/arma3/configs/{}"'.format(CONFIG_FILE, BASIC_CONFIG_FILE)
-
-launch += ' -port={} -name="{}" -profiles="/arma3/configs/profiles"'.format(
+    launch += ' -port={} -name="{}" -profiles="/arma3/configs/profiles"'.format(
     os.environ["PORT"], os.environ["ARMA_PROFILE"]
 )
 
-if os.path.exists("servermods"):
-    launch += mod_param("serverMod", local.mods("servermods"))
+    if os.path.exists("servermods"):
+        launch += mod_param("serverMod", local.mods("servermods"))
 
-print("LAUNCHING ARMA SERVER WITH", launch, flush=True)
-os.system(launch)
+
+
+    print("LAUNCHING ARMA SERVER WITH", launch, flush=True)
+    os.system(launch)
+
+else if OP_MODE.lower() = "headless":
+        
+    host_ip = ""
+    try: 
+        host_ip =IPv4Network(os.environ(HOST_IP))
+    catch:
+        print("[ERROR]: Valid IPv4 Host IP is required for headless mode. Exiting.")
+    else:
+        with open("/arma3/configs/{}".format(CONFIG_FILE)) as config:
+            data = config.read()
+            regex = r"(.+?)(?:\s+)?=(?:\s+)?(.+?)(?:$|\/|;)"
+
+            config_values = {}
+
+            matches = re.finditer(regex, data, re.MULTILINE)
+            for matchNum, match in enumerate(matches, start=1):
+                config_values[match.group(1).lower()] = match.group(2)
+
+            if "headlessclients[]" not in config_values:
+                data += '\nheadlessclients[] = {"127.0.0.1"};\n'
+            if "localclient[]" not in config_values:
+                data += '\nlocalclient[] = {"127.0.0.1"};\n'
+
+            with open("/tmp/arma3.cfg", "w") as tmp_config:
+                tmp_config.write(data)
+            launch += ' -config="/tmp/arma3.cfg" -cfg="/arma3/configs/{}"'.format(BASIC_CONFIG_FILE)
+
+        launch += " -client -connect={}".format(host_ip)
+        if "password" in config_values:
+            client_launch += " -password={}".format(config_values["password"])
+
+
+        launch += ' -name="{}-hc-{}"'.format(
+                os.environ["ARMA_PROFILE"], os.uname()[1]
+            )
+        print("LAUNCHING ARMA CLIENT {} WITH".format(i), launch)
+        os.system(launch)
+
+            
+        
